9c1473b268e1c5953c1974f1b707e86f0d5e6fd0
Davi Pessoa <davialvespessoa@gmail.com>
2025-03-12 23:10:49 -0300

    Links no footer do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,32 @@ export default function Page() {
         <aside></aside>
       </div>
 
-      <footer className="flex justify-between"></footer>
+      <footer className="flex justify-between">
+        <div className="flex gap-2">
+          <Link
+            href="https://www.linkedin.com/in/davipessoa93/"
+            target="_blank"
+            className="hover:text-muted-foreground"
+          >
+            <Linkedin className="h-5 w-5" />
+          </Link>
+          <Link
+            href="https://github.com/davipess93"
+            target="_blank"
+            className="hover:text-muted-foreground"
+          >
+            <Github className="h-5 w-5" />
+          </Link>
+        </div>
+
+        <Link
+          href="https://wa.me/5586998164238?text=Oi,%20Davi!%20Vi%20seu%20portfólio%20e%20curti%20bastante!%20Bora%20conversar?"
+          target="_blank"
+          className="underline hover:text-muted-foreground"
+        >
+          Vamos trabalhar juntos?
+        </Link>
+      </footer>
     </div>
   );
 }
