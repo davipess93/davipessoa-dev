b4250bdc5a7b684d86a4ff34de0f8b4e7c5d05f3
Davi Pessoa <davialvespessoa@gmail.com>
2025-03-12 21:49:25 -0300

    Cabeçalho do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { ThemeToggle } from "@/components/theme/theme-toggle";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-between p-4">
-      <h1>Hello, Davipessoa.dev!</h1>
+      <div className="flex items-center gap-2">
+        <Avatar>
+          <AvatarImage src="https://avatars.githubusercontent.com/u/47341160?v=4" />
+          <AvatarFallback>DP</AvatarFallback>
+        </Avatar>
+        <span>Oi, eu sou Davi Pessoa👋</span>
+      </div>
       <ThemeToggle />
     </div>
   );
