c888835e19e62d904e0a03ad5fed8ce43c81f752
Davi Pessoa <davialvespessoa@gmail.com>
2025-03-13 23:34:44 -0300

    Componentizando o header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
 import { DynamicSubtitle } from "@/components/dynamic-subtitle";
 import { DynamicTitle } from "@/components/dynamic-title";
 import { Footer } from "@/components/footer";
-import { ThemeToggle } from "@/components/theme/theme-toggle";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Header } from "@/components/header";
 
 export default function Page() {
   return (
     <div className="min-h-screen flex flex-col justify-between p-6 max-w-[1600px] m-auto">
       <div>
-        <div className="flex justify-between mb-6">
-          <div className="flex items-center gap-2">
-            <Avatar>
-              <AvatarImage src="https://avatars.githubusercontent.com/u/47341160?v=4" />
-              <AvatarFallback>DP</AvatarFallback>
-            </Avatar>
-            <span>Oi, eu sou Davi Pessoa👋</span>
-          </div>
-          <ThemeToggle />
-        </div>
+        <Header />
 
         <div className="grid grid-cols-1 lg:grid-cols-2">
           <main className="my-8 md:my-0 min-h-[120px]">
---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import { ThemeToggle } from "./theme/theme-toggle";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export function Header() {
+  return (
+    <div className="flex justify-between mb-6">
+      <div className="flex items-center gap-2">
+        <Avatar>
+          <AvatarImage src="https://avatars.githubusercontent.com/u/47341160?v=4" />
+          <AvatarFallback>DP</AvatarFallback>
+        </Avatar>
+        <span>Oi, eu sou Davi Pessoa👋</span>
+      </div>
+      <ThemeToggle />
+    </div>
+  );
+}
