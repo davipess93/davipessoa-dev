b32a4fffc8ce21d2156e24e4386ed3b81192aa5f
Davi Pessoa <davialvespessoa@gmail.com>
2025-03-17 10:06:34 -0300

    Ajustes na fonte do título para responsividade

--- src/components/dynamic-title.tsx
+++ src/components/dynamic-title.tsx
@@ -8,7 +8,7 @@ export function DynamicTitle() {
   return (
     <TypeAnimation
       sequence={['DESENVOLVEDOR\nWEB💻', 2000, 'DESENVOLVEDOR\nMOBILE📱', 2000]}
-      className={`mb-2 whitespace-pre-line text-center text-[40px]/[2.25rem] font-extrabold sm:text-6xl lg:text-left xl:text-7xl 2xl:text-[88px] ${jetBrainsMono.className}`}
+      className={`mb-2 whitespace-pre-line text-center text-[40px]/[2.5rem] font-extrabold sm:text-6xl lg:text-left xl:text-[84px]/[5.125rem] 2xl:text-[88px] ${jetBrainsMono.className}`}
       wrapper="h1"
       speed={50}
       repeat={Infinity}
