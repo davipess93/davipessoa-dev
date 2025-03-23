54308335232a299e719fe3d1d0704d4fa22e500a
Davi Pessoa <davialvespessoa@gmail.com>
2025-03-23 16:14:45 -0300

    Separando social links em componente

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,64 +1,12 @@
-'use client'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import * as brandIcons from '@fortawesome/free-brands-svg-icons'
-import { faQuestion } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
-import { HTMLAttributes, useEffect, useState } from 'react'
+import { HTMLAttributes } from 'react'
 
-import { websiteApi } from '@/lib/axios'
-
-import { Icon } from './icon'
-
-export type SocialLink = {
-  provider: string
-  url: string
-}
-
-type GetSocialLinksAPIResponse = {
-  socialLinks: SocialLink[]
-}
+import { SocialIconsLinks } from './social-icons-links'
 
 export function Footer({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
-  const allBrandIcons = Object.values(brandIcons).filter(
-    (icon) => typeof icon === 'object',
-  )
-  library.add(...allBrandIcons, faQuestion)
-
-  const [socialLinks, setSocialLinks] = useState<SocialLink[]>([])
-
-  useEffect(() => {
-    getSocialLinks()
-  }, [])
-
-  async function getSocialLinks() {
-    try {
-      const {
-        data: { socialLinks },
-      } = await websiteApi.get<GetSocialLinksAPIResponse>(
-        '/api/get-social-links',
-      )
-
-      setSocialLinks(socialLinks)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   return (
     <footer className={`flex justify-between ${className}`} {...rest}>
-      <div className="flex items-center gap-2">
-        {socialLinks.map((socialLink, index) => (
-          <Link
-            key={`${socialLink.provider}${index}`}
-            href={socialLink.url}
-            target="_blank"
-            className="hover:text-muted-foreground h-5"
-          >
-            <Icon url={socialLink.url} />
-          </Link>
-        ))}
-      </div>
+      <SocialIconsLinks />
 
       <Link
         href="https://wa.me/5586998164238?text=Oi,%20Davi!%20Vi%20seu%20portfólio%20e%20curti%20bastante!%20Bora%20conversar?"
---
+++ src/components/social-icons-links.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import {
+  IconName,
+  IconPrefix,
+  library,
+} from '@fortawesome/fontawesome-svg-core'
+import * as brandIcons from '@fortawesome/free-brands-svg-icons'
+import { faQuestion } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'lucide-react'
+import { useEffect, useState } from 'react'
+
+import { websiteApi } from '@/lib/axios'
+
+type SocialLink = {
+  provider: string
+  url: string
+}
+
+type GetSocialLinksAPIResponse = {
+  socialLinks: SocialLink[]
+}
+
+type FontAwesomeIconProps = [type: IconPrefix, icon: IconName]
+
+export function SocialIconsLinks() {
+  const domainToIconMap: Record<string, IconName> = {
+    google: 'google',
+    facebook: 'facebook',
+    twitter: 'twitter',
+    youtube: 'youtube',
+    github: 'github',
+    linkedin: 'linkedin',
+    instagram: 'instagram',
+  }
+
+  const allBrandIcons = Object.values(brandIcons).filter(
+    (icon) => typeof icon === 'object',
+  )
+
+  library.add(...allBrandIcons, faQuestion)
+
+  const [socialLinks, setSocialLinks] = useState<SocialLink[]>([])
+
+  useEffect(() => {
+    getSocialLinks()
+  }, [])
+
+  async function getSocialLinks() {
+    try {
+      const {
+        data: { socialLinks },
+      } = await websiteApi.get<GetSocialLinksAPIResponse>(
+        '/api/get-social-links',
+      )
+
+      setSocialLinks(socialLinks)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  function getSocialNetworkName(
+    socialNetworkLink: string,
+  ): FontAwesomeIconProps {
+    const regex = /https?:\/\/(?:www\.)?([^/.]+)/
+    const match = socialNetworkLink.match(regex)
+
+    if (!match) return ['fas', 'question']
+
+    const domainName = match[1]
+
+    return ['fab', domainToIconMap[domainName]]
+  }
+
+  return (
+    <div className="flex items-center gap-2">
+      {socialLinks.map((socialLink, index) => (
+        <Link
+          key={`${socialLink.provider}${index}`}
+          href={socialLink.url}
+          target="_blank"
+          className="hover:text-muted-foreground h-5"
+        >
+          <FontAwesomeIcon
+            style={{ width: 20, height: 20 }}
+            icon={getSocialNetworkName(socialLink.url)}
+          />
+        </Link>
+      ))}
+    </div>
+  )
+}
